58e5a8c98b9dfed7f97b13e95327ad4fe3b2b86d
Zubslav <54633356+Zubslav@users.noreply.github.com>
2019-08-29 13:06:48 +0300

    Update opt_shir.m

--- opt_shir.m
+++ opt_shir.m
@@ -1,3 +1,4 @@
+%Поиск оптимальной ширины полосы нечувствительности, при которой потери мнимальны
 clear; 
 clc;
 
@@ -7,7 +8,7 @@ d=0;
 d_array = zeros(1,m);
 q_array = zeros(1,m);
 for i=1:m
-    [p1 , L1, q, p2, L2] = test19([0 0 0], 1 , 0.2, d);
+    [p1 , L1, q, p2, L2] = Probability_norm_dist([0 0 0], 1 , 0.2, d);
     d=d+0.0001;
     d_array(1,i)=d;
     q_array(1,i)=q;
